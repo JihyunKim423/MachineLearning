bd9ff37b2a9114e8d6f4a24757b6cf7d7e836843
Jihyun Kim <156955284+JihyunKim423@users.noreply.github.com>
2024-04-05 11:10:35 -0700

    Update python3 train_jihyun.py

--- Llama2 instruction fine-tuning with Quora/python3 train_jihyun.py
+++ Llama2 instruction fine-tuning with Quora/python3 train_jihyun.py
@@ -2,4 +2,4 @@
 # Below is the shell command including my hf_token, path to the quora_dtaset and the target directory where the model will be saved
 ##############################################################################################
 
-python3 train_jihyun.py --hf_token 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 --data_path jbrophy123/quora_dataset --lora_dir jihyunkim423/quora_hw_dataset
+python3 train_jihyun.py --hf_token {YOUR_HF_TOKEN_HERE} --data_path jbrophy123/quora_dataset --lora_dir jihyunkim423/quora_hw_dataset
